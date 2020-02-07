b7e3638357e1095e41fcbec6c0629dc3d54198ed
rhaynesOU <raymondlhaynes@ou.edu>
2020-02-07 15:11:46 -0600

    Corrections

--- R/myRead.R
+++ R/myRead.R
@@ -9,6 +9,5 @@
 #' dird="C:\\Users\\stew9983\\OneDrive - University of Oklahoma\\DATAxls\\"
 #' mpg.df=myread("EPAGAS.csv")
 myread = function(csv){
-  fl = paste(dird,csv,sep="")
-  read.table(fl,header=TRUE,sep=",")
+  read.table(csv,header=TRUE,sep=",")
 }

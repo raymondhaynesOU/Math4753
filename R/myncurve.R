43e07252fb7d08c58fc0e4f14889b5a0ebd6e318
rhaynesOU <raymondlhaynes@ou.edu>
2020-02-21 11:48:35 -0600

    More Stuff

--- R/myncurve.R
+++ R/myncurve.R
@@ -1,11 +1,10 @@
 #' myncurve
 #'
-#' @param mu The mean for the normal distribution
-#' @param sigma The standard deviation for the normal distribution
-#' @param a The right endpoint for the probability of the distribution.
+#' @param mu The mean of the normal curve.
+#' @param sigma The sd of the normal curve
+#' @param a The endpoint for the probability.
 #'
-#' @return A normal plot with the area from -infinity to a shaded in
-#' and the probability of the x falling into that area.
+#' @return A normal curve with an area from -infinity to a shaded in.
 #' @export
 #'
 #' @examples
@@ -15,5 +14,6 @@ myncurve = function(mu, sigma, a){
   xcurve = seq((mu - 3 * sigma), a, length=1000)
   ycurve = dnorm(xcurve, mean = mu, sd = sigma)
   polygon(c(mu - 3*sigma, xcurve, a), c(0, ycurve, 0), col = "Red")
-  print(paste("P( X <= ", a, ") = ", pnorm(a, mean = mu, sd = sigma)), quote = FALSE)
+  print(paste("P( X <= ", a, ") = ",
+              pnorm(a, mean = mu, sd = sigma)), quote = FALSE)
 }
